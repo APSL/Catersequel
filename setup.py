00e26da0eec2ceb3d18e928a275af04a785125f7
Quique Piña <epina@apsl.net>
2018-07-20 09:22:17 +0200

    Added readme

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="catersequel",
-    version="0.2.1",
+    version="0.2.2",
     author="Enrique Pina Monserrat",
     author_email="lord.feratum@gmail.com",
     description="A simple raw SQL Python ORM",
     url="https://github.com/LordFeratum/Catersequel",
-    download_url="https://github.com/LordFeratum/Catersequel/archive/0.2.1.tar.gz",
+    download_url="https://github.com/LordFeratum/Catersequel/archive/0.2.2.tar.gz",
     keywords=['sql', 'orm', 'raw-sql', 'python'],
     packages=find_packages(),
 )
